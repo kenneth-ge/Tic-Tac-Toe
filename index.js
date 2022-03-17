f17b28605e87352745d3a2e529264b8c6f1da3d6
Kenneth Ge <kenge@student.edgemont.org>
2022-03-17 16:21:42 -0400

    added stuff

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-let express = require('express');
+ let express = require('express');
 let path = require('path')
 let router = require('./router')
 let fs = require('fs')
+let WebSocket = require('ws');
+let wss = new WebSocket.Server({ port: 7071 });
+
+let clients = new Map();
 
 const app = express();
 
@@ -22,6 +26,86 @@ app.use((req, res, next) => {
         reason: "Page Not Found",
         description: "The page you are looking for does not exist."
     })
+})
+
+let uuidv4 = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+var numPlayers = 0
+var currentPlayer = 0
+
+let lengthN = 5 //num rows
+let lengthM = 7 //num columns
+let board = new Array(lengthN)
+
+for(let i = 0; i < lengthN; i++){
+    board[i] = new Array(lengthM)
+    for(let j = 0; j < lengthM; j++){
+        board[i][j] = -1
+    }
+}
+
+wss.on('connection', (ws) => {
+    const id = uuidv4();
+    const playerNum = numPlayers
+    const metadata = { id, playerNum };
+
+    clients.set(ws, metadata);
+
+    ws.send(JSON.stringify({
+        yourPlayerNumber: numPlayers,
+        currentPlayer: currentPlayer,
+        board,
+        lengthN, lengthM
+    }))
+
+    numPlayers++
+
+    ws.on('message', (messageAsString) => {
+        const message = JSON.parse(messageAsString);
+        const metadata = clients.get(ws);
+  
+        message.sender = metadata.id;
+        message.playerNum = metadata.playerNum;
+
+        if(!message.squareX || !message.squareY){
+            console.log("Missing squareX and squareY")
+            return
+        }
+
+        console.log(message)
+
+        if(message.playerNum != currentPlayer){
+            console.log("Not your turn")
+            return;
+        }
+
+        board[message.squareY][message.squareX] = message.playerNum
+
+        message.playerWhoJustWent = currentPlayer
+
+        currentPlayer++
+        currentPlayer %= numPlayers
+
+        console.log(numPlayers)
+
+        message.currentPlayer = currentPlayer
+
+        clients.forEach((value, key) => {
+            message.yourPlayerNumber = value.playerNum
+            console.log("Send to player " + message.yourPlayerNumber)
+            const outbound = JSON.stringify(message);
+            key.send(outbound);
+        });
+    })
+
+    ws.on("close", () => {
+        clients.delete(ws);
+      });
 })
 
 app.listen(8080)
--- public/script.js
+++ public/script.js
@@ -1,25 +1,57 @@
 let height = window.innerHeight;
 let width = window.innerWidth;
 
-let lengthN = 5 //num rows
-let lengthM = 7 //num columns
-const numPlayers = 6;
-
-let currPlayer = 0
+var yourPlayerNumber = -1
+var currPlayer = -1
 
 //no piece -> 0
 //player 1 -> 1
 //player 2 -> 2
 //etc. 
 
-let board = new Array(lengthN)
 
-for(let i = 0; i < lengthN; i++){
-    board[i] = new Array(lengthM)
-    for(let j = 0; j < lengthM; j++){
-        board[i][j] = -1
-    }
-}
+let socket = new WebSocket("ws://localhost:7071/ws");
+
+socket.onopen = function(e) {
+  alert("[open] Connection established");
+  alert("Sending to server");
+};
+
+let lengthN = -1 //num rows
+let lengthM = -1 //num columns
+var board = [[0, 0], [0, 0]]
+socket.onmessage = function(event) {
+  data = JSON.parse(event.data)
+
+  if(yourPlayerNumber == -1){
+    yourPlayerNumber = data.yourPlayerNumber
+
+    board = data.board
+    lengthN = data.lengthN
+    lengthM = data.lengthM
+
+    drawValues()
+
+    alert(yourPlayerNumber)
+  }else{
+    alert(data)
+
+    currPlayer = data.currentPlayer
+    squareX = data.squareX
+    squareY = data.squareY
+    playerWhoJustWent = data.playerWhoJustWent
+
+    if(!squareX || !squareY || !playerWhoJustWent)
+      return;
+
+    board[squareY][squareX] = playerWhoJustWent
+  }
+};
+
+socket.onerror = function(error) {
+  alert(`[error] ${error.message}`);
+};
+
 printBoard();
 function setup() {
     createCanvas(width, height);
@@ -39,19 +71,27 @@ function setup() {
     drawValues()
   }
 
+  let serverapproval = false;
+
   function mouseClicked(){
       //check bounds
       //each box: x, x+(width/lengthN)  y, y+(height/lengthM)
-      squareX = int(mouseX/(width/lengthM))
-      squareY = int(mouseY/(height/lengthN))
-      console.log(squareX, squareY)
-      if(board[squareY][squareX] == -1){
-        board[squareY][squareX] = currPlayer
-        currPlayer = currPlayer+1
-        currPlayer %= numPlayers
+      if(currPlayer == yourPlayerNumber){
+        squareX = int(mouseX/(width/lengthM))
+        squareY = int(mouseY/(height/lengthN))
+        console.log(squareX, squareY)
+        if(board[squareY][squareX] == -1){
+          board[squareY][squareX] = currPlayer
+          socket.send(JSON.stringify({
+            squareX,
+            squareY
+          }));
+        }
+  
+        printBoard();
       }
 
-      printBoard()
+
   }
 
 let vals = ['X', 'O', 'Δ', '▢', '✂', '📪']
